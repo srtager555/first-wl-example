2e5ce497b1693e0c19b4cfb5b7ff42b3c3cd82f0
Carlos && Ponce <srtager555@gmail.com>
2022-10-27 17:06:45 -0600

    Adding styles to the container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const ContentContainer = styled.div`
+  height: 1000px;
+  width: 1000px;
+  background-color: #fff;
+`;
+
 function Home() {
-  return <p>Hola!</p>;
+  return (
+    <ContentContainer>
+      <p>a</p>
+    </ContentContainer>
+  );
 }
 
 export default Home;
