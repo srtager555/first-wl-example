dc4b622570268faf64d254bd5b8803e6d87843a4
Carlos && Ponce <srtager555@gmail.com>
2022-10-27 17:04:46 -0600

    Issue when the page is loaded solved

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { AppProps } from 'next/app'
-import { useEffect, useState } from 'react'
+import { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [load, setLoad] = useState(false)
+  const [load, setLoad] = useState(false);
 
-  useEffect(()=> {
-    setLoad(true)
-  }, [])
+  useEffect(() => {
+    setLoad(true);
+  }, []);
 
-  if (load) return;
+  if (!load) return;
 
   return <Component {...pageProps} />;
 }
