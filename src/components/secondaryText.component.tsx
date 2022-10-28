3bd529dc80677b9fa0d849d123816dc0726c37e7
Carlos && Ponce <srtager555@gmail.com>
2022-10-28 17:55:12 -0600

    I created the component to the description

---
+++ src/components/secondaryText.component.tsx
@@ -0,0 +1,11 @@
+import Wl from "wrapping-letters-react";
+
+import { Secondary } from "@styles/secondaryText.styles";
+
+export function SecondaryText({ text }: { text: string }) {
+  return (
+    <Secondary>
+      <Wl />
+    </Secondary>
+  );
+}
