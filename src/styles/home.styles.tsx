a07bdad17457f053df95e47236e2535f582f7884
Carlos && Ponce <srtager555@gmail.com>
2022-10-27 19:38:00 -0600

    I added new styles

--- src/styles/home.styles.tsx
+++ src/styles/home.styles.tsx
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
-export const ContentContainer = styled.div`
-  height: 1000px;
+export const Content = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  height: 900px;
   width: 1000px;
   background-color: #fff;
+  overflow: hidden;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: #ededed;
 `;
