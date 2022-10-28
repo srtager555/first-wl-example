4e9fb63eab4d5d059ab02a57d60fd78877038630
Carlos && Ponce <srtager555@gmail.com>
2022-10-27 19:38:58 -0600

    I started the new styles to the title

---
+++ src/styles/title.styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Title = styled.p`
+  font-family: "Bebas Neue";
+  font-size: 300px;
+  text-align: center;
+  line-height: 250px;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 35%,
+    rgba(0, 212, 255, 1) 100%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+`;
