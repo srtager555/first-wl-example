b63a756ea4929e21a850b99b15673ae922f9a1d9
Carlos && Ponce <srtager555@gmail.com>
2022-10-28 03:50:41 -0600

    I removed the BG clip

--- src/styles/title.styles.tsx
+++ src/styles/title.styles.tsx
@@ -5,14 +5,6 @@ export const Title = styled.p`
   font-size: 300px;
   text-align: center;
   line-height: 250px;
-  background: rgb(2, 0, 36);
-  background: linear-gradient(
-    90deg,
-    rgba(2, 0, 36, 1) 0%,
-    rgba(9, 9, 121, 1) 35%,
-    rgba(0, 212, 255, 1) 100%
-  );
-  background-clip: text;
-  -webkit-background-clip: text;
+
   color: transparent;
 `;
