588db2282ebe5b72601f5f1d08ba1bba2581e2cb
Carlos && Ponce <srtager555@gmail.com>
2022-10-28 00:31:01 -0600

    a

--- src/components/title.component.tsx
+++ src/components/title.component.tsx
@@ -2,7 +2,7 @@ import WrappingLetters from "wrapping-letters-react";
 
 import { Title as TitleStyle } from "@styles/title.styles";
 
-export function Title({ text }) {
+export function Title({ text }: { text: string }) {
   return (
     <TitleStyle>
       <WrappingLetters text={text} />
