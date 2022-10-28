e4d2340853370e4d7248f35da75e4a4b64acb602
Carlos && Ponce <srtager555@gmail.com>
2022-10-28 03:51:00 -0600

    I created a new component to the BG clip

--- src/styles/home.styles.tsx
+++ src/styles/home.styles.tsx
@@ -2,13 +2,26 @@ import styled from "styled-components";
 
 export const Content = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
   flex-direction: column;
   height: 900px;
   width: 1000px;
   background-color: #fff;
   overflow: hidden;
+`;
+export const Background = styled.div`
+  background: rgb(0, 212, 255);
+  background: linear-gradient(
+    0deg,
+    rgba(0, 212, 255, 1) 0%,
+    rgba(9, 9, 121, 1) 33%,
+    rgba(2, 0, 36, 1) 50%,
+    rgba(9, 9, 121, 1) 67%,
+    rgba(0, 212, 255, 1) 100%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
 `;
 
 export const Container = styled.div`
