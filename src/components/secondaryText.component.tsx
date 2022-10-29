1023b63837c124ecb7294e33abfd86b8f9d8d00b
Carlos && Ponce <srtager555@gmail.com>
2022-10-28 19:57:48 -0600

    I added a structure on Wl

--- src/components/secondaryText.component.tsx
+++ src/components/secondaryText.component.tsx
@@ -1,11 +1,24 @@
+import { LegacyRef, useEffect, useRef } from "react";
 import Wl from "wrapping-letters-react";
 
-import { Secondary } from "@styles/secondaryText.styles";
+import { Secondary, Span, SpanContainer } from "@styles/secondaryText.styles";
+
+function Custom({ letter }) {
+  return (
+    <SpanContainer>
+      <Span>{letter}</Span>
+    </SpanContainer>
+  );
+}
 
 export function SecondaryText({ text }: { text: string }) {
+  const ParentRef: LegacyRef<HTMLParagraphElement> = useRef(null);
+
+  useEffect(function () {}, []);
+
   return (
-    <Secondary>
-      <Wl text={text} />
+    <Secondary ref={ParentRef}>
+      <Wl text={text} structure={Custom} />
     </Secondary>
   );
 }
