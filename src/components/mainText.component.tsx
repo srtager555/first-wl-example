9ea82279e4a09d43bec211f59f12ec348cb1b8db
Carlos && Ponce <srtager555@gmail.com>
2022-10-28 23:03:28 -0600

    I created the animation to the mainText

--- src/components/mainText.component.tsx
+++ src/components/mainText.component.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef, LegacyRef } from "react";
+import { animation } from "./secondaryText.component";
 import WrappingLetters from "wrapping-letters-react";
 
-import { Title } from "@styles/title.styles";
+import { Span, Title } from "@styles/title.styles";
 
-export function MainText({ text }: { text: string }) {
+function Custom({ letter, top }) {
+  return <Span top={top}>{letter}</Span>;
+}
+
+export function MainText({
+  text,
+  top = false,
+}: {
+  text: string;
+  top?: boolean;
+}) {
+  const ParentRef: LegacyRef<HTMLParagraphElement> = useRef(null);
+
+  useEffect(function () {
+    const PRE_TARGETS: HTMLCollection = ParentRef.current.children;
+
+    const TARGETS = Array.from(PRE_TARGETS);
+
+    animation(TARGETS);
+  }, []);
+
   return (
-    <Title>
-      <WrappingLetters text={text} />
+    <Title ref={ParentRef}>
+      <WrappingLetters
+        text={text}
+        structure={{ structure: Custom, props: { top } }}
+      />
     </Title>
   );
 }
